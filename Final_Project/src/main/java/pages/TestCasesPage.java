aea905af9704151f4f1f3812ff7e31ccb27b9a88
og8002004 <20223185@fue.edu.eg>
2025-08-10 20:21:13 +0300

    Update TestCasesPage.java

--- Final_Project/src/main/java/pages/TestCasesPage.java
+++ Final_Project/src/main/java/pages/TestCasesPage.java
@@ -1,5 +1,31 @@
 package pages;
 
-public class TestCasesPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
+public class TestCasesPage extends PageBase {
+
+    public TestCasesPage(WebDriver driver) {
+		super(driver);
+	}
+
+	@FindBy(css = "h2[class='title text-center'] b")
+    private WebElement testCases;
+    public void TestCasesPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public WebElement getTestCases() {
+        
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOf(testCases));
+        return testCases;
+    }//.
 }
